990de2ebbbc6088ba0b317f7c143cb75443d853e
Olpas9 <66534811+Olpas9@users.noreply.github.com>
2021-09-23 13:26:20 +0300

    Update InstaOlo.py

--- InstaOlo.py
+++ InstaOlo.py
@@ -60,7 +60,7 @@ def check_proxy(q):
             opener.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11')]
             rq.install_opener(opener)
 
-            req = rq.Request('https://api.ipify.org/')
+            req = rq.Request('https://api64.ipify.org/')
 
             if rq.urlopen(req).read().decode() == proxy.partition(':')[0]:
                 proxys_working_list.update({proxy: proxy})
